8027cdac87867df80fb1d392b02bfd0f30026cd0
MIGUEL ANGEL PUENTES RAMIREZ <ma.puentes@sis.virtual.uniandes.edu.co>
2018-09-08 17:30:59 -0500

    Se agregaron nuevos metodos y atributos a las clases Mensaje y Cliente

--- src/main/Cliente.java
+++ src/main/Cliente.java
@@ -2,7 +2,64 @@ package main;
 
 public class Cliente extends Thread
 {
-	private int numeroMensajes;
+	/**
+	 * Identificador de cada uno de los clientes.
+	 */
+	private int id;
 	
+	/**
+	 * Atributo que representa el buffer al que tiene acceso
+	 * cada cliente.
+	 */
+	
+	private Buffer bf;
+	
+	
+	/**
+	 * Cantidad de mensajes que posee un cliente.
+	 */
+	private int numMsj;
+	
+	public Cliente(Buffer bf, int id, int numMsj)
+	{
+		this.id = id;
+		this.bf = bf;
+		this.numMsj = numMsj;
+	}
+	
+	public void run()
+	{
+		
+	}
+
+	public int darId()
+	{
+		return this.id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public Buffer getBf()
+	{
+		return bf;
+	}
+
+	public void setBf(Buffer bf)
+	{
+		this.bf = bf;
+	}
+
+	public int getNumMsj()
+	{
+		return numMsj;
+	}
+
+	public void setNumMsj(int numMsj)
+	{
+		this.numMsj = numMsj;
+	}
 	
 }
--- src/main/Mensaje.java
+++ src/main/Mensaje.java
@@ -2,5 +2,56 @@ package main;
 
 public class Mensaje
 {
+	/**
+	 * Identificador de cada mensaje.
+	 */
+	private int id;
 
+	/**
+	 * Mensaje en texto.	
+	 */
+	private String msj;
+
+	
+	/**
+	 * Estado del mensaje: 0 (enviado) v 1 (no enviado). 
+	 */
+	private int estado;
+	
+	public Mensaje(int id, String msj)
+	{
+		this.id = id;
+		this.msj = msj;
+		this.estado = 0;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getMsj()
+	{
+		return msj;
+	}
+
+	public void setMsj(String msj)
+	{
+		this.msj = msj;
+	}
+
+	public int getEstado()
+	{
+		return estado;
+	}
+
+	public void setEstado(int estado)
+	{
+		this.estado = estado;
+	}
 }
