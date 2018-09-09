275b5363f0cde2484421ff33dad251032ffdc61e
Iung Matthieu <iungmatthieu@MacBook-Pro-de-Iung-2.local>
2018-09-09 15:46:47 -0500

    Buffer constructor

--- src/main/Buffer.java
+++ src/main/Buffer.java
@@ -1,5 +1,17 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Buffer {
+
+    private int nCliente;
+    private int tamanoBuffer;
+    private ArrayList losMensajes;
+
+    public Buffer (int nCli, int tamBuf){
+        nCliente = nCli;
+        tamanoBuffer = tamBuf;
+        losMensajes = new ArrayList<Mensaje>();
+    }
 
 }
