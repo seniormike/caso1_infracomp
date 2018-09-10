568d5722e0bf28eb643f29d5b3067adb247d01dd
Iung Matthieu <iungmatthieu@MacBook-Pro-de-Iung-2.local>
2018-09-10 15:01:20 -0500

    buffer metodo enviar

--- src/main/Buffer.java
+++ src/main/Buffer.java
@@ -6,16 +6,37 @@ public class Buffer {
 
     private int nCliente;
     private int tamanoBuffer;
+    private Object lleno, vacio; 
     private ArrayList<Mensaje> losMensajes;
 
     public Buffer (int nCli, int tamBuf){
+        this.lleno = new Object();
+        this.vacio = new Object();
         this.nCliente = nCli;
         this.tamanoBuffer = tamBuf;
         this.losMensajes = new ArrayList<Mensaje>();
     }
 
-    public void enviar(int msj){
-        
+    public void enviar(Mensaje msj) {
+        synchronized(lleno){
+            while (losMensajes.size() == tamanoBuffer){
+                try {
+                    lleno.wait();
+                    Thread.yield(); 
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        synchronized(this){
+            losMensajes.add(msj);
+            System.out.println("la cantidad de mensaje en el buffer :  "+ losMensajes.size());
+        }
+
+        synchronized(vacio){
+            vacio.notify();
+        }
+
     }
 
 }
