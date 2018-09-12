495e4622235166f53eea96c0b50f5b1ebe86ae69
Miguel Puentes <ma.puentes@uniandes.edu.co>
2018-09-11 23:56:49 -0500

    Se desarrolla la clase que carga el archivo de propiedades

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,69 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.swing.JFileChooser;
+
+
 public class Main
 {
+
+	/*
+		numero.clientes=10
+		numero.servidores=2
+		numero.consultas.cliente=5
+		buffer.size=4
+	 */
+	public static int numClientes;
+	public static int numServidores;
+	public static int numConsultas;
+	public static int bufferSize;
+
+	/**
+	 * Variable que representa el archivo del cual se va a leer la informacion.
+	 */
+	public static File prop;
+
+
+
+
+	public static void cargar()
+	{
+		JFileChooser fc = new JFileChooser("./data");
+		int resultado = fc.showOpenDialog(fc );
+
+		if( resultado == JFileChooser.APPROVE_OPTION)
+		{
+			prop = fc.getSelectedFile( );
+		}
+		Properties p = new Properties();
+
+		try {
+			p.load(new FileReader(fc.getSelectedFile().getPath()));
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+		numClientes = Integer.parseInt(p.getProperty("numero.clientes"));
+		numServidores = Integer.parseInt(p.getProperty("numero.servidores"));	
+		numConsultas = Integer.parseInt(p.getProperty("numero.consultas.cliente"));
+		bufferSize = Integer.parseInt(p.getProperty("buffer.size"));	
+		
+	}
+
+	public static void main(String[] args)
+	{
+		cargar();
+	}
 
 }
